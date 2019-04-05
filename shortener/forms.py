ba76ed65d3185679fe4af2b299efea911ddf82d9
kamkulas <kam.kulas@gmail.com>
2019-04-05 11:51:24 +0200

    Little code style changes

--- shortener/forms.py
+++ shortener/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class URLForm(forms.Form):
-    url = forms.URLField(widget=forms.URLInput(attrs={'placeholder': 'Paste your URL here', 'class': 'url-field'}))
+    url = forms.URLField(widget=forms.URLInput(
+        attrs={'placeholder': 'Paste your URL here', 'class': 'url-field'}))
